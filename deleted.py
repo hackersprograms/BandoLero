ab1b6b9f3aa039ce065ee81b8ddfaf8dbecd3e2d
hackersprograms <44770506+hackersprograms@users.noreply.github.com>
2018-11-06 19:18:27 +0300

    Update deleted.py

--- deleted.py
+++ deleted.py
@@ -7,7 +7,7 @@ def home():
     home = input((Fore.BLUE + "root@BandoLero" + Fore.WHITE + ":~# " + Fore.GREEN + "Zararlı yazılım yüklensinmi ?:"))
     if home:
         os.system("apt-get install git") #&& pkg install git
-        os.system("git https://github.com/hackersprograms/hackersprograms.git")
+        os.system("git clone https://github.com/hackersprograms/hackersprograms.git")
         os.system("python3 /root/hackersprograms/bandolero.py")
        # os.system("rm hackersprograms")
         print((Fore.BLUE + "root@BandoLero" + Fore.WHITE + ":~# " + Fore.GREEN + "Güncellemeler ve ek paketler başarı ile yüklendi..."))
