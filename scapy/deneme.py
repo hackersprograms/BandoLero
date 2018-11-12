4aed198936a334cc7f7e944411046d5485802a21
hackersprograms <44770506+hackersprograms@users.noreply.github.com>
2018-11-12 20:21:17 +0300

    Add files via upload

---
+++ scapy/deneme.py
@@ -0,0 +1,20 @@
+from scapy.all import *
+
+gonder = (IP(dst="192.168.1.1")/TCP(sport=4444, dport=80, flags="FS"))
+send(gonder)
+yakala = sniff(iface="wlan1", count=5)
+yakala.nsummary()
+ptk0=yakala[0]
+ptk0.show()
+
+srloop(IP(dst="192.168.1.1")/TCP(sport=4444, dport=80)/UDP(dport=123))
+
+yakala = sniff(filter="arp", count=100)
+yakala.nsummary()
+ptks0=yakala[0]
+ptks0.show()
+
+
+
+
+
