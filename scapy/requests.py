df83d26a8babdb6803960cf1eb4df4302e45fb30
BandoLero <44770506+hackersprograms@users.noreply.github.com>
2020-06-23 04:21:15 +0300

    Update requests.py

--- scapy/requests.py
+++ scapy/requests.py
@@ -1,20 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("http://wowturkey.com/") ("linkteki kodları görmeye yarar")
-r.content ("linkteki kodları ekrana bastırır")
+r = requests.get("http://wowturkey.com/")   #linkteki kodları görmeye yarar
+r.content   #linkteki kodları ekrana bastırır
 
 soup = BeautifulSoup(r.content)
-print(soup.prettify()) ("linkteki kodları dünzenleyip ekrana basar")
+print(soup.prettify())  #linkteki kodları dünzenleyip ekrana basar
 
-soup.find_all("a") ("sayfadaki bütün linkleri çeker")
+soup.find_all("a")  #sayfadaki bütün linkleri çeker
 
-linkler = soup.find_all("a") ("linkleri tek tek basar")
+linkler = soup.find_all("a")    #linkleri tek tek basar
 for link in linkler:
     print(link)
 
 for link in linkler:
-    print(link.get<"href">) ("sayfada kaç link var onu görmeye yarar")
+    print(link.get<"href">)     #sayfada kaç link var onu görmeye yarar"
 
 for link in linkler: 
     print(link.text,link.get<"href>")
